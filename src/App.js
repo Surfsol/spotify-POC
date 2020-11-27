95811280a35e4979448c682cbcd0a5e9abbd8a25
Surfsol <surfsol@protonamil.com>
2020-11-27 17:43:53 -0300

    spotify

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+//allow to interact with spotify
+import SpotifyWebApi from "spotify-web-api-js";
+import { useStateValue } from "./StateProvider";
+import Player from "./Player";
+import { getTokenFromResponse } from "./spotify";
+import "./App.css";
+import Login from "./Login";
+
+const spotifyApi = new SpotifyWebApi();
 
 function App() {
+  //bring in token to switch between login and player
+  const [{ token }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // Set token
+    const hash = getTokenFromResponse();
+    //after get access token, clear from url
+    window.location.hash = "";
+    let _token = hash.access_token;
+
+    if (_token) {
+      spotifyApi.setAccessToken(_token);
+
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+
+      spotifyApi.getPlaylist("37i9dQZEVXcJZyENOWUFo7").then((response) =>
+        dispatch({
+          type: "SET_DISCOVER_WEEKLY",
+          discover_weekly: response,
+        })
+      );
+
+      spotifyApi.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
+
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: spotifyApi,
+      });
+
+      //get user
+      spotifyApi.getMe().then((user) => {
+        console.log('show user', user)
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+
+      spotifyApi.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        });
+      });
+    }
+  }, [token, dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!token && <Login />}
+      {token && <Player spotify={spotifyApi} />}
     </div>
   );
 }
